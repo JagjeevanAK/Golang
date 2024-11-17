927227773291831686de9febb5100b2a36cc60ae
Jagjeevan <83103469+JagjeevanAK@users.noreply.github.com>
2024-11-17 08:38:43 +0530

    init

--- re-learning/practice/t5/main.go
+++ re-learning/practice/t5/main.go
@@ -3,8 +3,15 @@ package main
 import "fmt"
 
 func main() {
+    //Array
 	var intarr [3]int;
 	fmt.Println(intarr[0]);
     fmt.Println(intarr[1:3]);
 
+    //Slice
+    var intslice = []int{1,2,3};
+    fmt.Println(intslice);
+
+    intslice = append(intslice,4,5);
+    fmt.Println(intslice);
 }
