b789fd428a03d01ffb89cc06efebdf8acb8d8d71
Jagjeevan <83103469+JagjeevanAK@users.noreply.github.com>
2025-05-18 07:36:17 +0530

    learnt about error's
    Signed-off-by: Jagjeevan <83103469+JagjeevanAK@users.noreply.github.com>

--- Re-learning/err/err_test.go
+++ Re-learning/err/err_test.go
@@ -7,7 +7,7 @@ func TestWallet(t *testing.T){
 	wallet.Deposit(20);
 
 	got := wallet.Balance()
-	want := 20.0
+	var want Bitcoin = 20.0
 
 	if got != want {
 		t.Errorf("Got %f instead of %f", got, want)
